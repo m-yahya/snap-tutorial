d6871db706248712efe3248a018e7fb0a8a076fb
m-yahya <yahya_gis@hotmail.com>
2020-02-17 13:21:22 +0100

    fix path

--- olibox_core/olibox_core/olibox_pkg/write_json.py
+++ olibox_core/olibox_core/olibox_pkg/write_json.py
@@ -10,7 +10,8 @@ def write_values(file, new_data):
     """
     The function uses json.dump method to write the data object to a file.
     """
-    file_to_write = os.path.join(snap_userdata, '/', file)
+    file_to_write = os.path.join(f'{snap_userdata}/{file}')
+    print(file_to_write)
     if os.path.isfile(file_to_write):
         with open(file_to_write) as f:
             data = json.load(f)
