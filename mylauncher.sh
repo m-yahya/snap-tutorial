20fc195d487af3b951ef3bb1e260672418b75df5
m-yahya <yahya_gis@hotmail.com>
2020-02-17 11:05:35 +0100

    Add contents to json files

--- mylauncher.sh
+++ mylauncher.sh
@@ -2,8 +2,8 @@
 
 [ -e "$SNAP_USER_DATA/config.toml" ] || touch $SNAP_USER_DATA/config.toml
 
-[ -e "$SNAP_USER_DATA/energy-data.json" ] || touch $SNAP_USER_DATA/energy-data.json
+[ -e "{}" > "$SNAP_USER_DATA/energy-data.json" ] || touch $SNAP_USER_DATA/energy-data.json
 
-[ -e "$SNAP_USER_DATA/power-data.json" ] || touch $SNAP_USER_DATA/power-data.json
+[ -e "{}" > "$SNAP_USER_DATA/power-data.json" ] || touch $SNAP_USER_DATA/power-data.json
 
 exec "$@"
